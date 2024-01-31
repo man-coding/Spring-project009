07bae46476b61fc8d9e24be173c628e7edfb2d0a
man-coding <chch2857@gmail.com>
2024-02-01 08:54:49 +0900

    ghh

---
+++ src/main/java/com/example/demo/dto/MemberDTO.java
@@ -0,0 +1,25 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberDTO {
+
+	String Id;
+	String password;
+	String name;
+	LocalDateTime regDate;
+	LocalDateTime modDate;
+}
